208f164bec963847be7234011e66d4b9aa02f5e5
Christian Weyer <christian.weyer@thinktecture.com>
2017-09-25 12:31:12 +0200

    Server code

---
+++ server/BASTAMBALive/Controllers/Dish.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BASTAMBALive.Controllers
+{
+    public class Dish
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        // Image
+        // List of Ingredients
+        // ....
+    }
+}
---
+++ server/BASTAMBALive/Controllers/DishesController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BASTAMBALive.Controllers
+{
+    [Route("api/[controller]")]
+    public class DishesController : Controller
+    {
+        private static ConcurrentDictionary<Guid, Dish> _database;
+
+        static DishesController()
+        {
+            _database = new ConcurrentDictionary<Guid, Dish>();
+            var guid = Guid.NewGuid();
+
+            _database.TryAdd(guid, new Dish
+            {
+                Id = guid,
+                Name = "Spaghetti Bolognese",
+                Description = " El classico!"
+            });
+        }
+
+        [HttpGet]
+        [Route("")]
+        public List<Dish> GetDishes()
+        {
+            return _database.Values.ToList();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetDish(Guid id)
+        {
+            Dish returnValue;
+
+            if(_database.TryGetValue(id, out returnValue))
+            {
+                return Ok(returnValue);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        [Route("")]
+        public Dish AddNewDish([FromBody]Dish dish)
+        {
+            var guid = Guid.NewGuid();
+
+            dish.Id = guid;
+
+            _database.TryAdd(guid, dish);
+
+            return dish;
+        }
+     }
+}
